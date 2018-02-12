d5fa06815cb9d632e7a02b07136c04ba9e2c8a15
Quentin Guimard <quentin.guimard@etu.unice.fr>
2018-02-12 11:19:41 +0100

    PopUp update

--- src/main/java/fr/polytech/ihm/controller/PopUpController.java
+++ src/main/java/fr/polytech/ihm/controller/PopUpController.java
@@ -1,4 +1,23 @@
 package fr.polytech.ihm.controller;
 
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.input.MouseEvent;
+
 public class PopUpController {
+    @FXML
+    private Button ok_button;
+
+    @FXML
+    private Button new_declaration_button;
+
+    @FXML
+    void new_declaration_button_clicked(MouseEvent event) {
+
+    }
+
+    @FXML
+    void ok_button_clicked(MouseEvent event) {
+
+    }
 }
