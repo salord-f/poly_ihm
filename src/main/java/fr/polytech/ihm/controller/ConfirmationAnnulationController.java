6e56ebaec46a18eb77949da6aae6eda9fa94a6a0
Florian Salord <florian.salord@etu.unice.fr>
2018-02-20 08:34:22 +0100

    Fix for going back to viwIncidents from popups

--- src/main/java/fr/polytech/ihm/controller/ConfirmationAnnulationController.java
+++ src/main/java/fr/polytech/ihm/controller/ConfirmationAnnulationController.java
@@ -6,31 +6,26 @@ import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
-import javafx.scene.control.Label;
-import javafx.scene.image.Image;
-import javafx.scene.image.ImageView;
-import javafx.scene.layout.GridPane;
-import javafx.scene.layout.HBox;
-import javafx.scene.layout.VBox;
+import javafx.stage.Modality;
 import javafx.stage.Stage;
 
 public class ConfirmationAnnulationController {
 
-    @FXML
-    private Button YesButton;
+	@FXML
+	private Button YesButton;
 
-    @FXML
-    private Button NoButton;
+	@FXML
+	private Button NoButton;
 
-    @FXML
-    public void NoButtonAction(ActionEvent event) {
-        Stage stage = (Stage) YesButton.getScene().getWindow();
-        stage.close();
-    }
+	@FXML
+	public void NoButtonAction(ActionEvent event) {
+		Stage stage = (Stage) YesButton.getScene().getWindow();
+		stage.close();
+	}
 
-    @FXML
-    public void YesButtonAction(ActionEvent event){ //TODO
-        try {
+	@FXML
+	public void YesButtonAction(ActionEvent event) { //TODO
+        /*try {
             Stage stage = (Stage) YesButton.getScene().getWindow();
 
             Stage stage2 = (Stage) stage.getOwner();
@@ -43,7 +38,25 @@ public class ConfirmationAnnulationController {
             stage.show();
         } catch (Exception e) {
             e.printStackTrace();
-        }
-    }
+        }*/
+
+		try {
+			Stage stage2 = (Stage) YesButton.getScene().getWindow();
+
+			Stage stage = new Stage();
+
+
+			Parent root = FXMLLoader.load(
+					getClass().getResource("/fxml/viewIncidents.fxml"));
+			stage.setScene(new Scene(root));
+			stage.setTitle("Liste des incidents déclarés");
+
+			stage.initModality(Modality.WINDOW_MODAL);
+			stage.initOwner(stage2);
+			stage.show();
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
 
 }
