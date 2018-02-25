2afa8d55ad008dfe14b288c55680e4ec39dd8aad
Calvin Audier <calvin.audier@etu.unice.fr>
2018-02-25 21:49:57 +0100

    Suppression FadeTransition variable

--- src/main/java/fr/polytech/ihm/controller/IncidentController.java
+++ src/main/java/fr/polytech/ihm/controller/IncidentController.java
@@ -22,7 +22,6 @@ import java.io.File;
 public class IncidentController {
     private Incident incident;
     private VoirDescriptionTransition t;
-    private FadeTransition fade;
 
     @FXML
     private Label date;
