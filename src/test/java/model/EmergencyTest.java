6c0239d9706765dfca18d74833dedfebe29dac5a
Marion Marguerettaz <marion.marguerettaz@etu.unice.fr>
2018-02-25 15:27:52 +0100

    tests emergency

---
+++ src/test/java/model/EmergencyTest.java
@@ -0,0 +1,19 @@
+package model;
+
+import fr.polytech.ihm.model.Emergency;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+
+public class EmergencyTest {
+
+    @Test
+    public void returnTheGoodEmergency(){
+        assertEquals(Emergency.HIGH, Emergency.values()[3]);
+        assertEquals(Emergency.MEDIUM, Emergency.values()[2]);
+        assertEquals(Emergency.LOW, Emergency.values()[1]);
+        assertEquals(Emergency.NONE, Emergency.values()[0]);
+    }
+}
